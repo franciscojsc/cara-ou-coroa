981e28be9164201a61333f765c9fc6d92e59109f
Francisco Chaves <francisco.jschaves@gmail.com>
2019-05-03 02:35:10 -0300

    Passando informação da moeda para tela resultado

--- app/src/main/java/br/com/franciscochaves/caraoucoroa/MainActivity.java
+++ app/src/main/java/br/com/franciscochaves/caraoucoroa/MainActivity.java
@@ -1,13 +1,41 @@
 package br.com.franciscochaves.caraoucoroa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView botaoJogar;
+    private String[] opcao = {"cara", "coroa"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
+
+        botaoJogar = findViewById(R.id.image_btn_jogar);
+
+        botaoJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Random random = new Random();
+                int numeroAleatorio = random.nextInt(2);//0-1
+
+                Intent intent = new Intent(MainActivity.this, DetalheActivity.class);
+                intent.putExtra("opcao", opcao[numeroAleatorio]);
+
+                startActivity(intent);
+
+            }
+        });
     }
 }
