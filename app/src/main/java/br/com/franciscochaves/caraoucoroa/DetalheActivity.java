6c9038733c731be5da45087fd0f435a00934b9ad
Francisco Chaves <francisco.jschaves@gmail.com>
2019-05-03 02:58:44 -0300

    Capturando a informação e exibindo a imagem correspondente

--- app/src/main/java/br/com/franciscochaves/caraoucoroa/DetalheActivity.java
+++ app/src/main/java/br/com/franciscochaves/caraoucoroa/DetalheActivity.java
@@ -2,12 +2,35 @@ package br.com.franciscochaves.caraoucoroa;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class DetalheActivity extends AppCompatActivity {
+
+    private ImageView imagem;
+    private ImageView botaoVoltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_detalhe);
+
+        imagem = findViewById(R.id.image_moeda_cara_coroa);
+        botaoVoltar = findViewById(R.id.image_botao_voltar);
+
+        String opcao = getIntent().getStringExtra("opcao");
+        if(opcao.equals("coroa")){
+            imagem.setImageResource(R.drawable.moeda_coroa);
+        }else{
+            imagem.setImageResource(R.drawable.moeda_cara);
+        }
+
+        botaoVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
